7f42a7d6c8857e339c65bdf72aae2f9f4e53b973
Jae <jae.duk.seo@ryerson.ca>
2019-04-13 20:00:09 -0400

    Super important commit

---
+++ whitening/whiten.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def whiten(X, method='zca'):
+    """
+    Whitens the input matrix X using specified whitening method.
+    Inputs:
+        X:      Input data matrix with data examples along the first dimension
+
+        method: Whitening method. Must be one of 'zca', 'zca_cor', 'pca',
+                'pca_cor', or 'cholesky'.
+    """
+    X = X.reshape((-1, np.prod(X.shape[1:])))
+    X_centered = X - np.mean(X, axis=0)
+    Sigma = np.dot(X_centered.T, X_centered) / X_centered.shape[0]
+    W = None
+    
+    if method in ['zca', 'pca', 'cholesky']:
+        U, Lambda, _ = np.linalg.svd(Sigma)
+        if method == 'zca':
+            W = np.dot(U, np.dot(np.diag(1.0 / np.sqrt(Lambda + 1e-5)), U.T))
+        elif method =='pca':
+            W = np.dot(np.diag(1.0 / np.sqrt(Lambda + 1e-5)), U.T)
+        elif method == 'cholesky':
+            W = np.linalg.cholesky(np.dot(U, np.dot(np.diag(1.0 / (Lambda + 1e-5)), U.T))).T
+    elif method in ['zca_cor', 'pca_cor']:
+        V_sqrt = np.diag(np.std(X, axis=0))
+        P = np.dot(np.dot(np.linalg.inv(V_sqrt), Sigma), np.linalg.inv(V_sqrt))
+        G, Theta, _ = np.linalg.svd(P)
+        if method == 'zca_cor':
+            W = np.dot(np.dot(G, np.dot(np.diag(1.0 / np.sqrt(Theta + 1e-5)), G.T)), V_sqrt)
+        elif method == 'pca_cor':
+            W = np.dot(np.dot(np.diag(1.0/np.sqrt(Theta + 1e-5)), G.T), V_sqrt)
+    else:
+        raise Exception('Whitening method not found.')
+
+    return np.dot(X_centered, W.T)
---
+++ whitening/zca.py
@@ -0,0 +1,47 @@
+"""
+MIT License
+
+Copyright (c) 2018 Ludovic Trottier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import numpy as np
+from sklearn.utils import as_float_array
+from sklearn.base import TransformerMixin, BaseEstimator
+
+class ZCA(BaseEstimator, TransformerMixin):
+
+    def __init__(self, regularization=1e-5, copy=False):
+        self.regularization = regularization
+        self.copy = copy
+
+    def fit(self, X, y=None):
+        X = as_float_array(X, copy=self.copy)
+        self.mean_ = np.mean(X, axis=0)
+        X = X - self.mean_
+        sigma = np.dot(X.T, X) / (X.shape[0] - 1)
+        U, S, V = np.linalg.svd(sigma)
+        tmp = np.dot(U, np.diag(1 / np.sqrt(S + self.regularization)))
+        self.components_ = np.dot(tmp, U.T)
+        return self
+
+    def transform(self, X):
+        X_transformed = X - self.mean_
+        X_transformed = np.dot(X_transformed, self.components_.T)
+        return X_transformed
