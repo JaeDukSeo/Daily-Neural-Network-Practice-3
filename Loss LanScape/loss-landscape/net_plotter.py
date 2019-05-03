291e6b942a3072811fa0b4f3c52b0f8c1d090ce6
Jae <jae.duk.seo@ryerson.ca>
2019-05-03 14:34:13 -0400

    Super important commit

--- Loss LanScape/loss-landscape/net_plotter.py
+++ Loss LanScape/loss-landscape/net_plotter.py
@@ -99,8 +99,9 @@ def normalize_direction(direction, weights, norm='filter'):
         # filter has the same norm as its corresponding filter in 'weights'.
         for d, w in zip(direction, weights):
             # print(d.shape)
+            print(count)
             d.mul_(w.norm()/(d.norm() + 1e-10))
-
+            count = count + 1
         import sys
         sys.exit()
     elif norm == 'layer':
