1779a6879472cd7890ef6cdd9ca5798195f4050c
Jae <jae.duk.seo@ryerson.ca>
2019-05-03 14:26:52 -0400

    Super important commit

--- Loss LanScape/loss-landscape/net_plotter.py
+++ Loss LanScape/loss-landscape/net_plotter.py
@@ -15,8 +15,6 @@ import model_loader
 def get_weights(net):
     """ Extract parameters from net, and return a list of tensors"""
     return [p.data for p in net.parameters()]
-
-
 def set_weights(net, weights, directions=None, step=None):
     """
         Overwrite the network's weights with a specified list of tensors
@@ -38,8 +36,6 @@ def set_weights(net, weights, directions=None, step=None):
 
         for (p, w, d) in zip(net.parameters(), weights, changes):
             p.data = w + torch.Tensor(d).type(type(w))
-
-
 def set_states(net, states, directions=None, step=None):
     """
         Overwrite the network's state_dict or change it along directions with a step size.
@@ -62,16 +58,12 @@ def set_states(net, states, directions=None, step=None):
             v.add_(d.type(v.type()))
 
         net.load_state_dict(new_states)
-
-
 def get_random_weights(weights):
     """
         Produce a random direction that is a list of random Gaussian tensors
         with the same shape as the network's weights, so one direction entry per weight.
     """
     return [torch.randn(w.size()) for w in weights]
-
-
 def get_random_states(states):
     """
         Produce a random direction that is a list of random Gaussian tensors
@@ -79,13 +71,9 @@ def get_random_states(states):
         per weight, including BN's running_mean/var.
     """
     return [torch.randn(w.size()) for k, w in states.items()]
-
-
 def get_diff_weights(weights, weights2):
     """ Produce a direction from 'weights' to 'weights2'."""
     return [w2 - w for (w, w2) in zip(weights, weights2)]
-
-
 def get_diff_states(states, states2):
     """ Produce a direction from 'states' to 'states2'."""
     return [v2 - v for (k, v), (k2, v2) in zip(states.items(), states2.items())]
